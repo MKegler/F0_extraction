beb99ebea24bbf523169530f2c79b08dde331f78
MKegler <mikolajkegler@gmail.com>
2019-04-12 14:21:04 +0100

    Added example FW

--- Main.m
+++ Main.m
@@ -1,7 +1,8 @@
 clear all
 close all
 
-fnames = dir('./SoundFiles/*.wav');
+% fnames = dir('./SoundFiles/*.wav');
+fnames = dir('./SoundFiles/story.wav');
 SavePath = char(fullfile('./FWs'));
 
 for fi = 1:size(fnames,1)
@@ -10,7 +11,7 @@ fileName = strcat('./SoundFiles/', fnames(fi).name);
 speechfile = char(fullfile(fileName));
 disp(fileName);
 
-F0 = extractFW(speechfile,1);
+F0 = extractFW(speechfile);
 
 savename = strcat(fnames(fi).name(1:end-4), '_F0.mat');
 fullFileName = fullfile(SavePath(1,:), savename);
